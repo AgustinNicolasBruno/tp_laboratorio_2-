e276165c7b9763f6d6157b42a1072eb0165920e7
AgustinNicolasBruno <agustin.n.bruno@gmail.com>
2017-07-12 22:54:00 -0300

    Add files via upload

---
+++ TP-02/Entidades/Dulce.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string patente, ConsoleColor color):base(patente,marca,color)
+        {
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        /// <summary>
+        /// Muestra todos los datos del producto y el dulce
+        /// </summary>
+        /// <returns></returns>
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_2017;
+
+namespace Entidades_2017
+{
+    public class Snacks:Producto
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Muesta datos del producto y los snacks
+        /// </summary>
+        /// <returns></returns>
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
